c90d2cfcb38bba5527e6af8f2f526615359af6ff
ChoiWheatley <chltmdgus604@naver.com>
2022-09-13 16:08:56 +0900

    override toString

--- ch6/Initializer.java
+++ ch6/Initializer.java
@@ -3,17 +3,9 @@ package ch6;
 public class Initializer {
   public static void main(String[] args) {
     var car = new Car();
-    System.out.println(toString(car));
+    System.out.println(car);
   }
 
-  static String toString(final Car car) {
-    String ret = "[ ";
-    ret += "color: " + car.color + ", ";
-    ret += "gearType: " + car.gearType + ", ";
-    ret += "door: " + car.door + ", ";
-    ret += " ]";
-    return ret;
-  }
 }
 
 class Car {
@@ -38,5 +30,15 @@ class Car {
 
   /* copy ctor */ Car(Car other) {
     this(other.color, other.gearType, other.door);
+  }
+
+  @Override /* 뭔가 이렇게 쓰는 걸 본 적이 있다. */
+  public String toString() { /* 사실 얘는 Object 메서드로, 오버라이딩 하면 println같은 함수에 객체를 집어넣어도 된다. */
+    String ret = "[ ";
+    ret += "color: " + this.color + ", ";
+    ret += "gearType: " + this.gearType + ", ";
+    ret += "door: " + this.door + ", ";
+    ret += " ]";
+    return ret;
   }
 }
