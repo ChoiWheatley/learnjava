311ed48a368dde2a32c4a8a0e2e27ee528ec7989
ChoiWheatley <chltmdgus604@naver.com>
2022-09-14 20:32:15 +0900

    explicit super ctor

---
+++ ch7/SuperInit.java
@@ -0,0 +1,45 @@
+package ch7;
+
+public class SuperInit {
+  public static void main(String[] args) {
+    new Point3D(1, 2, 3);
+  }
+}
+
+class Point2D {
+  int x, y;
+
+  Point2D(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  String getLocation() {
+    return "{x: " + x + ", y: " + y + "}";
+  }
+}
+
+class Point3D extends Point2D {
+  int z;
+
+  /**
+   * 생성자를 정의하지 않으면 아래와 같은 컴파일 에러를 얻게된다.
+   * Implicit super constructor Point() is undefined for default constructor. Must
+   * define an explicit constructor
+   * 암묵적 super 생성자 Point()가 정의되지 않았습니다. 명시적 생성자를 정의하세요.
+   */
+  Point3D(int x, int y, int z) {
+    /**
+     * super() 생성자를 호출하지 않으면 아래와 같은 컴파일 에러를 얻게된다.
+     * Implicit super constructor Point() is undefined. Must explicitly invoke
+     * another constructor
+     */
+    super(x, y);
+    this.z = z;
+  }
+
+  @Override
+  String getLocation() {
+    return "{x: " + x + ", y: " + y + ", z: " + z + "}";
+  }
+}
