45b828401eb30bf174a2adf090d162a2ae0531a1
ChoiWheatley <chltmdgus604@naver.com>
2022-10-10 16:47:46 +0900

    Chained exception 실습, 실제 사용 예시

--- ch8/ChainedException.java
+++ ch8/ChainedException.java
@@ -1,14 +1,12 @@
 package ch8;
 
-import javax.management.RuntimeErrorException;
-
 public class ChainedException {
   public static void main(String[] args) {
     /** checked 예외발생 */
     // throw new SomeCheckedException("msg"); // 컴파일 타임에 걸린다. 따라서 강제적으로 try-catch 사용
 
     /** checked 예외를 unchecked로 둔갑 */
-    throw new RuntimeException(new SomeCheckedException("Message"));
+    throw new RuntimeException(new SomeCheckedException("This is a checked exception"));
   }
 }
 
